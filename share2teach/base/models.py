28f3291997fe33356096a352fd92893ab3034abc
Bonolo Zizi <bonolozizi7@gmail.com>
2024-08-23 15:37:28 +0200

    A commit

--- share2teach/base/models.py
+++ share2teach/base/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class subject(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Document(models.Model):
+    title = models.CharField(max_length=255)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    contributor = models.ForeignKey(Contributor, on_delete=models.SET_NULL, null=True)
+    file = models.FileField(upload_to='documents/')
+    upload_date = models.DateTimeField(auto_now_add=True)
+
+class Rating(models.Model):
+    document = models.ForeignKey(Document, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField(null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+
--- share2teach/base/views.py
+++ share2teach/base/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth import login as auth_login
 from django.conf import settings
 from django.shortcuts import redirect
 from django.views import View
+from django.shortcuts import render, get_object_or_404
+from .models import Document, Rating
+from .forms import RatingForm
 
 def home(request):
     return render(request, 'home.html')
@@ -17,3 +20,16 @@ def register(request):
 def login(request):
     # Your registration logic here
     return render(request, 'login.html')
+
+def home_view(request):
+    documents = Document.objects.all()
+    return render(request, 'home.html', {'documents': documents})
+
+def document_detail_view(request, document_id):
+    document = get_object_or_404(Document, id=document_id)
+    form = RatingForm(request.POST or None)
+    if form.is_valid():
+        rating = form.save(commit=False)
+        rating.document = document
+        rating.save()
+    return render(request, 'document_detail.html', {'document': document, 'form': form})
